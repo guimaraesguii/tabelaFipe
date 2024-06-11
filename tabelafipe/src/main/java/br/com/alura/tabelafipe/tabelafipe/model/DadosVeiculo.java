6db75cabe97a59475f68e0e67f0c415256defb51
guimaraesguii <guifereler@gmail.com>
2024-06-11 02:36:13 -0300

    finishing application

--- tabelafipe/src/main/java/br/com/alura/tabelafipe/tabelafipe/model/DadosVeiculo.java
+++ tabelafipe/src/main/java/br/com/alura/tabelafipe/tabelafipe/model/DadosVeiculo.java
@@ -1,18 +1,2 @@
-package br.com.alura.tabelafipe.tabelafipe.model;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class DadosVeiculo {@JsonAlias("codigo") Integer codigo;
-                           @JsonAlias("nome") String nome;
-
-    @Override
-    public String toString() {
-        return "DadosVeiculo{" +
-                "codigo=" + codigo +
-                ", nome='" + nome + '\'' +
-                '}';
-    }
+package br.com.alura.tabelafipe.tabelafipe.model;public record DadosVeiculos() {
 }
-
---
+++ tabelafipe/src/main/java/br/com/alura/tabelafipe/tabelafipe/model/Veiculo.java
@@ -0,0 +1,2 @@
+package br.com.alura.tabelafipe.tabelafipe.model;public record Veiculo() {
+}
